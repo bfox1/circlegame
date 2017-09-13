051fd6f2c30104e7c1d6f5e7f3a16081a1871146
bfox1 <robert.gamiz@yahoo.com>
2017-09-13 00:00:59 -0700

    Running simple tests.
    prepping canvas area and experimenting

--- CircleGameDeluxeJS/src/io/github/bfox1/circlegamedeluxe/CircleGame.js
+++ CircleGameDeluxeJS/src/io/github/bfox1/circlegamedeluxe/CircleGame.js
@@ -1,21 +1,47 @@
 
+var container = document.getElementById("myContainer");
+
+var x = container.offsetWidth;
+var y = container.offsetHeight;
 
 function moveSquare()
 {
     var elem = document.getElementById("myAnimation");
-    var pos = 0;
+    var posX = 0;
+    var posY = 0;
+    var xSpeed = 3;
+    var ySpeed = 5;
     var id = setInterval(frame, 10);
 
     function frame()
     {
-        if(pos == 350)
+
+
+        if(posX < 0)
         {
-            clearInterval(id);
-        }else
-        {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
+            posX = 0;
+            xSpeed = Math.abs(xSpeed);
         }
+        else if(posX > x - 50)
+        {
+            posX = x - 50;
+            xSpeed = -Math.abs(xSpeed)
+        }
+        if(posY < 0)
+        {
+            posY = 0;
+            ySpeed = Math.abs(ySpeed);
+        }
+        else if(posY > y - 50)
+        {
+            posY = y - 50;
+            ySpeed = -Math.abs(ySpeed);
+        }
+        posX += xSpeed;
+        posY += ySpeed;
+
+        elem.style.top = posX + 'px';
+        elem.style.left = posY + 'px';
+
     }
 }
